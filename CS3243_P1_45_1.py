c3862e880aa99d08b533a1890b0395fd3d815ca3
David Livingston <ddl.tdh@gmail.com>
2020-05-17 16:37:55 +0800

    initial optimisation

--- CS3243_P1_45_1.py
+++ CS3243_P1_45_1.py
@@ -5,86 +5,83 @@ import os
 import sys
 from collections import deque
 import copy
-import operator
 
 # Running script on your own - given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
 
+class Node(object):
+    def __init__(self, curr_state, parent, action, pos):
+        self.state = curr_state
+        self.parent = parent
+        self.action = action
+        self.pos = pos
 class Puzzle(object):
-    class Node(object):
-        def __init__(self, curr_state, parent, action):
-                self.state = curr_state
-                self.parent = parent
-                self.action = action
-
     def __init__(self, init_state, goal_state):
         # you may add more attributes if you think is useful
         self.init_state = init_state
         self.goal_state = goal_state
-        self.actions = list()
+        self.actions = deque()
         for i, v in enumerate(self.init_state):
             for j, k in enumerate(v):
                 if k == 0:
                     self.start_pos = (i, j)
                     break
+        self.start_node = Node(self.init_state, None, None, self.start_pos)
     
-    
-    # def swap(self, curr_state, pos, direction, visited):
-    #     if direction == 'UP':
-    #         temp = curr_state[pos[0]][pos[1]]
-    #         curr_state[pos[0]][pos[1]] = curr_state[pos[0]][pos[1] - 1]
-    #         curr_state[pos[0]][pos[1] - 1] = temp
-    #     elif direction == 'DOWN':
-    #     elif direction == 'LEFT':
-    #     elif direction == 'RIGHT':
-
-    # def isNextStateVisited(self, curr_state, pos, action, visited):
+    def swap(self, curr_state, pos, direction):
+        temp = curr_state[pos[0]][pos[1]]
+        curr_state[pos[0]][pos[1]] = curr_state[pos[0]+direction[0]][pos[1] + direction[1]]
+        curr_state[pos[0]+direction[0]][pos[1]+direction[1]] = temp
+        return curr_state
     
     def move(self, curr_state, pos, visited, direction):
         if pos[0] + direction[0] >= len(curr_state) or pos[0] + direction[0] < 0 or pos[1] + direction[1] >= len(curr_state[0]) or pos[1] + direction[1] < 0:
             return None, pos
-        next_state = copy.deepcopy(curr_state)
-        temp = next_state[pos[0]][pos[1]]
-        next_state[pos[0]][pos[1]] = next_state[pos[0]+direction[0]][pos[1] + direction[1]]
-        next_state[pos[0]+direction[0]][pos[1]+direction[1]] = temp
-        if str(next_state) in visited: # can optimise by not deepcopying when next state is already visited
+        if str(self.swap(curr_state, pos, direction)) in visited:
+            curr_state = self.swap(curr_state, pos, direction)
             return None, pos
+        
+        next_state = copy.deepcopy(curr_state)
+        curr_state = self.swap(curr_state, pos, direction)
         visited.add(str(next_state))
         return next_state, (pos[0]+direction[0], pos[1]+direction[1])
-        
+
 
     def solve(self):
-        moves = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-        directions = [(0,1), (1, 0), (-1, 0), (0, -1)]
+        move_directions = {
+            (0,1): 'LEFT',
+            (1,0): 'UP',
+            (-1, 0): 'DOWN',
+            (0, -1): 'RIGHT'
+        }
+        isFound = False
         visited = set()
         q = deque()
-        
-        def isVisited(curr_state):
-            return str(curr_state) in visited
 
         visited.add(str(self.init_state))
-        q.append((self.init_state, self.start_pos))
+        q.append(self.start_node)
 
         while q:
-            curr_state, pos = q.popleft()
-    #         with open('log.txt', 'a') as f:
-    #             f.write('curr state: \n' )
-    #             f.write('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-    #   for row in curr_state]))
-    #             f.write('\n---------\n')
+            curr_node = q.popleft()
+            curr_state, pos = curr_node.state, curr_node.pos
             if curr_state == self.goal_state:
-                return True
+                isFound = True
+                break
             
-            for direction in directions:
+            for direction in move_directions.keys():
                 next_state, next_pos = self.move(curr_state, pos, visited, direction)
                 if next_state and next_pos:
-                    q.append((next_state, next_pos))
+                    q.append(Node(next_state, curr_node, move_directions[direction], next_pos))
         
-        return False
-            
-                # think of how to represent the state transition
-
-    # you may add more functions if you think is useful
+        if isFound:
+            while curr_node:
+                if curr_node.action:
+                    self.actions.appendleft(curr_node.action)
+                curr_node = curr_node.parent
+        else:
+            return ['UNSOLVABLE']
+        
+        return self.actions
 
 if __name__ == "__main__":
     # do NOT modify below
@@ -133,12 +130,5 @@ if __name__ == "__main__":
     ans = puzzle.solve()
 
     with open(sys.argv[2], 'a') as f:
-        # for answer in ans:
-            f.write(str(ans))
-
-
-
-
-
-
-
+        for answer in ans:
+            f.write(answer+'\n')
